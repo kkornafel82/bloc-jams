6692966441c3ce421677aa71f56f761ac7630417
Kevin <kkornafel82@yahoo.com>
2016-02-02 20:12:00 -0800

    assignment 22 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,20 @@ var animatePoints = function() {
 
    var points = document.getElementsByClassName('point');
 
-   var revealFirstPoint = function() {
+   
+   var revealPoint = function(points) {
+      for (var i=0;i<points.length;i++) {
+       points[i].style.opacity = 1;
+       points[i].style.transform = "scaleX(1) translateY(0)";
+       points[i].style.msTransform = "scaleX(1) translateY(0)";
+       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+      }
+
+   };
+
+   revealPoint(points);
+
+   /*var revealFirstPoint = function() {
        points[0].style.opacity = 1;
        points[0].style.transform = "scaleX(1) translateY(0)";
        points[0].style.msTransform = "scaleX(1) translateY(0)";
@@ -25,7 +38,9 @@ var animatePoints = function() {
 
    revealFirstPoint();
    revealSecondPoint();
-   revealThirdPoint();
+   revealThirdPoint();*/
+
+
 
 };
       
