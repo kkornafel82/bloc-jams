d4a4c4244c7cb20655e86f3eef9d2f91d5c65f65
Kevin <kkornafel82@yahoo.com>
2016-02-10 20:10:05 -0800

    assignment 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -65,11 +65,25 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     var currentParent = element.parentElement;
-    while (currentParent.className != targetClass) {
+
+    if (currentParent) {
+      while ( currentParent.className && currentParent.className != targetClass) {
         currentParent = currentParent.parentElement;
+      }
+
+      if (currentParent.className == targetClass) {
+        return currentParent;
+      } else {
+        alert("No parent with that class name found");
+      }
+    
+    } else {
+      alert("No parent found");
     }
+    
     return currentParent;
 };
+
 
 
 var getSongItem = function(element) {
